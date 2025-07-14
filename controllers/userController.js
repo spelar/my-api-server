8cbcabc4fc2b2fbcc870233438a9cb1fb367652a
Sungsu Kim <spelar7@gmail.com>
2025-07-15 00:24:22 +0900

    feat: 회원가입 API 구현 및 MySQL 연동
    - MySQL user 테이블 생성
    - .env 환경변수로 DB 연결 정보 관리
    - 회원가입 API 라우트, 컨트롤러, 모델 구현
    - 회원가입 성공/에러 응답 처리

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,17 @@
 const userModel = require('../models/userModel');
 
 exports.register = async (req, res) => {
-  // 회원가입 로직은 추후 구현
-  res.send('회원가입 API');
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).json({ message: '필수 항목이 누락되었습니다.' });
+  }
+  userModel.registerUser(email, password, name, (err, result) => {
+    if (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(409).json({ message: '이미 등록된 이메일입니다.' });
+      }
+      return res.status(500).json({ message: '회원가입 중 오류가 발생했습니다.' });
+    }
+    res.status(201).json({ message: '회원가입 성공', userId: result.insertId });
+  });
 };
--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const port = 4000;
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(express.json());
+app.use('/user', userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
