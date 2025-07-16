f328805fb9f5f4d163a7a78c911fae1084018e68
Sungsu Kim <spelar7@gmail.com>
2025-07-17 00:03:05 +0900

    feat: 회원가입 CORS 문제 해결
    - CORS 미들웨어를 라우터 등록 전에 적용하여 프론트엔드 연동 문제 해결
    - TypeScript 환경에서 cors import 방식으로 변경 및 타입 에러 해결
    - DB 회원 데이터 정상 저장 확인

--- index.ts
+++ index.ts
@@ -2,16 +2,16 @@
 import express, { Application } from 'express';
 import userRouter from './routes/user';
 import dotenv from 'dotenv';
-const cors = require('cors');
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 const port = 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/user', userRouter);
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
